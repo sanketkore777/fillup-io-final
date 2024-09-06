df4e620d754a4d12c38afda9b4e91c06561d6502
Sanket Kore <131244558+sanketkore777@users.noreply.github.com>
2024-09-06 21:12:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
