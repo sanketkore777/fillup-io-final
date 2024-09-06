6c3c3eb3f3eac158d1d26a1f2e0cc8e92aa50623
Sanket Kore <131244558+sanketkore777@users.noreply.github.com>
2024-09-06 11:10:57 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,9 +10,9 @@ export default function NotFound() {
           Sorry, the page you're looking for doesn't exist or has been moved.
         </p>
         <Link href="/">
-          <a className="inline-block px-6 py-3 text-lg font-medium text-white bg-blue-600 rounded-full hover:bg-blue-700 transition duration-300">
+          <span className="inline-block px-6 py-3 text-lg font-medium text-white bg-blue-600 rounded-full hover:bg-blue-700 transition duration-300">
             Return to Home
-          </a>
+          </span>
         </Link>
       </div>
       <div className="mt-10">
